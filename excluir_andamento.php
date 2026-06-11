b8ff50807a3c00bd47ede716dfb41d392b87ec50
Rafael Gustavo <88495051+RafaelGustavo45@users.noreply.github.com>
2026-06-11 13:36:59 -0300

    Delete excluir_andamento.php
    rs

--- excluir_andamento.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'conexao.php';
-if (isset($_GET['id'])) {
-    $stmt = $pdo->prepare("DELETE FROM andamento WHERE id = ?");
-    $stmt->execute([$_GET['id']]);
-}
-header("Location: andamento.php");
